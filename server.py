b86365271b1aa3832340e2ca149d62c293c39c00
Brook Kebede <brookabyss@gmail.com>
2017-06-08 13:18:33 -0700

    Ninjas populated, change ninjas based on color in the url

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect, render_template, session
+app=Flask(__name__)
+app.secret_key="12345"
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/ninja')
+def show():
+    return render_template('ninja.html')
+@app.route('/ninja/<color>')
+def show_color(color):
+    if color=="blue":
+        session['ninja']= "img/leonardo.jpg"
+    elif color=="orange":
+        session['ninja']= "img/michelangelo.jpg"
+    elif color=="red":
+        session['ninja']= "img/raphael.jpg"
+    elif color=="purple":
+        session['ninja']= "img/donatello.jpg"
+    return render_template('color.html')
+app.run(debug=True)
